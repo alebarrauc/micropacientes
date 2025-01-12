908d8399d9686ecd624fb510c17ad9361401b9c6
Alexis <ale.barra@duocuc.cl>
2025-01-12 20:43:48 -0300

    JWT Integration Sucess

--- src/main/java/com/gespaciente/micro_pacientes/config/SecurityConfig.java
+++ src/main/java/com/gespaciente/micro_pacientes/config/SecurityConfig.java
@@ -4,16 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()) // Deshabilita la protección CSRF
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()); // Permite el acceso a todos los endpoints sin autenticación
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/pacientes").permitAll() // Deja público /api/pacientes
+                .anyRequest().authenticated() // Todo lo demás requiere autenticación
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://login.microsoftonline.com/145f8758-681d-4413-9137-2973073f86e2/discovery/v2.0/keys").build();
     }
 }
